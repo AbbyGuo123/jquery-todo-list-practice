3ced8397b723e48255f854a74863e329af018525
GUOAB <AbbyGuo@oocl.com>
2018-07-17 21:23:46 +0800

    add click (all 、active、complete) function

--- js/todo.js
+++ js/todo.js
@@ -26,41 +26,72 @@ function generateUUID() {
     return uuid;
 }
 
-const list = [{id:"c57aab79-7dfa-4d85-8ede-aa653a8b5d93",value:"Parking Lot APP Refactor",class:"",Checked:false},
-            {id:"00bd6227-e881-4024-ad7a-4212d930c947",value:"Parking Lot APP 时序图",class:"",Checked:true},
-            {id:"8a134a9d-1df9-475a-ba5c-e27f9f003a99",value:"Parking Lot APP 中类的流程图",class:"",Checked:false},
-            {id:"af51c0e0-97a4-4e78-9a31-48763fd6c089",value:"总结Java和面向对象相关的概念和知识",class:"",Checked:true},
-            {id:"cda424c4-7f16-4841-886a-0de60a395d34",value:"总结Parking Lot APP开发过程中的问题和经验",class:"",Checked:false}];
+const list = [{id:"c57aab79-7dfa-4d85-8ede-aa653a8b5d93",value:"Parking Lot APP Refactor",class:""},
+            {id:"00bd6227-e881-4024-ad7a-4212d930c947",value:"Parking Lot APP 时序图",class:"checked"},
+            {id:"8a134a9d-1df9-475a-ba5c-e27f9f003a99",value:"Parking Lot APP 中类的流程图",class:""},
+            {id:"af51c0e0-97a4-4e78-9a31-48763fd6c089",value:"总结Java和面向对象相关的概念和知识",class:"checked"},
+            {id:"cda424c4-7f16-4841-886a-0de60a395d34",value:"总结Parking Lot APP开发过程中的问题和经验",class:""}];
+
+let showType='all';
+
 function addItem(){
     let name = $('input[name=ListItem]').val();
     let id = generateUUID();
     list.push({id:id.toString(),value:name,class:"",Checked:false});
-    buildHtml();
     render();
     $('input[name=ListItem]').val("");
 }
 
+function checkCheckBox(itemId){
+    for(let i=0;i<list.length;i++){
+        if(list[i].id==itemId){
+            list[i].class = "checked";
+        }
+    }
+    render();
+}
 
+function clickActive() {
+    showType = 'checked';
+    $("a[data-filter=active]").addClass('selected');
+    buildHtml();
+    render();
 
+}
+function clickComplete() {
+    showType = '';
+    buildHtml();
+    render();
+}
+
+function clickAll() {
+    showType = 'all';
+    buildHtml();
+    render();
+
+}
 
 function buildHtml(){
-    let todoList = `
-        <ol >`;
+    let todoList =`<ol >`;
+
     for(let item of list){
-        todoList+=`<li id=${item.id} class=${item.Checked}>
-                <input name="done-todo" type="checkbox" class="done-todo"> ${item.value} </li>`;
+        if(showType==='all'||showType===item.class){
+            todoList+=`<li id=${item.id} class=${item.class}>
+                <input name="done-todo" type="checkbox" class="done-todo" onclick="checkCheckBox('${item.id}')" ${item.class}> ${item.value} </li>`;
+        }
+
     }
         todoList+=`</ol>
         <div>
             <ul id="filters">
                 <li>
-                    <a href="#" data-filter="all" class="selected">ALL</a>
+                    <a href="#" data-filter="all" class="selected" onclick="clickAll()">ALL</a>
                 </li>
                 <li>
-                    <a href="#" data-filter="active" class="">Active</a>
+                    <a href="#" data-filter="active" class="" onclick="clickActive()">Active</a>
                 </li>
                 <li>
-                    <a href="#" data-filter="complete" class="">Complete</a>
+                    <a href="#" data-filter="complete" class="" onclick="clickComplete()">Complete</a>
                 </li>
             </ul>
 
@@ -71,5 +102,5 @@ function buildHtml(){
 }
 
 const render = ()=>{
-    $('#todoForm').html(buildHtml());
+    $('#todoForm').html(buildHtml);
 }
